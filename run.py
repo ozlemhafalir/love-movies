b3e9207726606a37736564d829bd0043aadc11af
ozlemhafalir <ozlemoztep@gmail.com>
2022-12-15 23:29:45 +0000

    Initial commit: Install Cinemagoer and confirm searching movie

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from imdb import Cinemagoer
+
+
+ia = Cinemagoer()
+
+results = ia.search_movie("harry potter")
+for result in results:
+    print(result.data["title"])
