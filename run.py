8ee73eb1b2e18b594d0493a2d408924da57f4032
ozlemhafalir <ozlemoztep@gmail.com>
2022-12-16 20:47:56 +0000

    Add more formatting to messages

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def get_random_movies(list, count=20):
 def get_does_user_like_movie(movie):
     """Helper function to ask if user likes the movie, parse and return boolean value."""
     movie_basic_info = MovieBasicInfo(movie)
-    answer = input(f"[{movie_basic_info.year}] {movie_basic_info.title} \n")
+    answer = input(f"> [{movie_basic_info.year}] {movie_basic_info.title} \n")
     while answer != "y" and answer != "n":
         answer = input(
             f"Please answer with y or n. Do you like: [{movie_basic_info.year}] {movie_basic_info.title} \n"
@@ -53,11 +53,14 @@ def suggest_movies(liked_genres, liked_movies_count):
     top_genres_list = [
         genre_item[0] for genre_item in list(top_genres_dict.items())[:3]
     ]
-    print(
-        f"\nYou liked {liked_movies_count} movies. Your favorite genres are: {', '.join(top_genres_list)}"
-    )
+    print(f"""
+----------------------------------------------------------------------
+You liked {liked_movies_count} movies.
+Your favorite genres are: {', '.join(top_genres_list)}
+Here are some suggestions for you:
+----------------------------------------------------------------------
+""")
     top_genre_movies = ia.get_top50_movies_by_genres(top_genres_list)
-    print("\nHere are some suggestions for you")
     for genre_movie in top_genre_movies[:10]:
         movie_basic_info = MovieBasicInfo(genre_movie)
         print(f"[{movie_basic_info.year}] {movie_basic_info.title}")
@@ -67,28 +70,30 @@ def start_game():
     """
     Game logic
     """
+    input("""
+----------------------------------------------------------------------
+Welcome! You will be asked with 20 random top 250 IMDB movies.
+Answer with y if like the movie, answer with n otherwise.
+Press ENTER/RETURN to continue.
+----------------------------------------------------------------------
+\n""")
     movies = ia.get_top250_movies()
     random_movies = get_random_movies(movies)
-    input(
-        """
-----------------------------------------------------------------------
-Welcome! You will be asked with [year] movie.
-Answer with y if you know and like the movie, answer with n otherwise.
-Press return to continue.
-----------------------------------------------------------------------
-\n\
-        """
-    )
     liked_movies_count = 0
-    liked_genres = dict()
+    liked_genres = {}
     for movie in random_movies:
         if get_does_user_like_movie(movie):
             liked_movies_count += 1
-            print("Adding genres to your list, please wait...\n")
+            print("* adding genres to your list, please wait...")
             genres = get_genres_of_movie(movie)
             for genre in genres:
                 liked_genres[genre] = liked_genres.get(genre, 0) + 1
+        print("")
     suggest_movies(liked_genres, liked_movies_count)
+    
+    answer = input("\nDo you want to try again? (y/n): ")
+    if answer == "y":
+        start_game()
 
 
 def main():
